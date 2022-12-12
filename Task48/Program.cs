b3978b7734b64c23d0ee9742da3c71783f6cc089
Андрей Ковалев <kovalevandrey007@gmail.com>
2022-12-12 22:10:36 +0200

    2

---
+++ Task48/Program.cs
@@ -0,0 +1,59 @@
+﻿/*Задача 48: Задайте двумерный массив размера m на n, 
+каждый элемент в массиве находится 
+по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5 */
+
+
+//1.напечатать массив
+//2.получить m
+//3.задать матрицу и заполнить ее числами
+
+//1.получить числа с консоли 
+int GetNumber(string message) 
+{ 
+    int result = 0; 
+    while(true)
+    { 
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result)) 
+        { 
+            break; 
+        } 
+        else { Console.WriteLine("Ввели не число"); 
+        } 
+    } return result;
+ } 
+
+//2. матрицу и заполнить ее числами 
+int[,] GetMatrix(int m, int n) 
+{ 
+    int[,] matrix = new int[m,n]; 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+     { 
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        { 
+            matrix[i, j ] = i + j; 
+        } 
+    } return matrix; 
+}
+
+ //3.распечатать матрицу 
+ void PrintMatrix(int[,] matrix) 
+ { 
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    { 
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+             Console.Write($"{matrix[i, j]} "); 
+        }
+              Console.WriteLine(); 
+     } 
+ } 
+              
+int m = GetNumber("Введите количество строк:"); 
+int n = GetNumber("Введите количество столбцов"); 
+int[,] matrix = GetMatrix(m,n); 
+PrintMatrix(matrix); 
---
+++ Task51/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Runtime.Serialization;
+/*Задача 51: Задайте двумерный массив. Найдите сумму 
+элементов, находящихся на главной диагонали 
+(с индексами (0,0); (1;1) и т.д.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12 */
+
+
+//1. функция ввода числа
+//2. функция задачи рандомного массива
+//3. функция печати массива
+//4.  функция ождения суммы элементов на главной диагонали
+
+//1. функция ввода числа
+int GetNumber(string text) 
+{ 
+int result = 0; 
+Console.WriteLine(text); 
+while(true) 
+{ 
+if (int.TryParse(Console.ReadLine(), out result)) 
+{ 
+break; 
+} 
+} 
+return result; 
+} 
+//функция задачи рандомного 
+int[,]GetMatrix(int m, int n) 
+{ 
+int [,] matrix = new int [m, n]; 
+Random rnd = new Random(); 
+for (int i = 0; i < matrix.GetLength(0); i++) 
+{ 
+for (int j = 0; j < matrix.GetLength(1); j++) 
+{ 
+matrix[i, j] = rnd.Next(0,3); 
+} 
+} 
+return matrix; 
+} 
+
+//3. функция печати массива
+void PrintMatrix(int[,] matrix) 
+ { 
+    for (int i = 0; i < matrix.GetLength(0); i++) 
+    { 
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+             Console.Write($"{matrix[i, j]} "); 
+        }
+              Console.WriteLine(); 
+     } 
+ } 
+//4.  функция ождения суммы элементов на главной диагонали
+int FindSum (int [,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i< matrix.GetLength(0); i++ )
+    {
+        for (int j =0; j< matrix.GetLength(0); j++ )
+        {
+        sum +=  matrix[i, j];   
+        }
+    }
+     return sum;
+}
+
+//Вызываем функции
+int numer=  GetNumber("введите количество строк: ");
+int nor =  GetNumber("введите количество столбцов: ");
+int [,] tort = GetMatrix(numer,  nor);
+PrintMatrix(tort);
+int popil = FindSum (tort);
+Console.WriteLine($"сумма ={popil}");
+ 
